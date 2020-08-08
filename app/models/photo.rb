7db982475b188c2f5c5ddea6801972898b52ec3c
leonard percival <leonardpercival@gmail.com>
2020-08-08 02:28:49 +0100

    go

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,7 @@ class Photo < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   has_one_attached :photo
+  validates :photo, size: { less_than: 2.megabytes, message: 'cannot exceed 2MB' }
   validates :photo, attached: true
   validates :title, presence: true
   validates :alt, presence: true
