10da34ab73f5061c7a53de5285b05035fe2d1ed3
leonard percival <leonardpercival@gmail.com>
2020-08-08 04:51:23 +0100

    dd

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,6 @@ class Event < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   validates :title, presence: true
-  validates :content, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
   has_many_attached :photos
